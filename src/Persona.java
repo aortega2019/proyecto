a9011df565238f83d8b6340c77749e6217aad3f0
Adrian <williampeleas@gmail.com>
2019-05-23 09:25:20 +0200

    inicial

---
+++ src/Persona.java
@@ -0,0 +1,43 @@
+
+public class Persona {
+
+	private String nombre, apellidos;
+	private int edad;
+	
+	public Persona() {
+		super();
+		// TODO Apéndice de constructor generado automáticamente
+	}
+	
+	public Persona(String nombre, String apellidos, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+}
